e0e7ed6d44797705b6c494866902997ba4f36754
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-11-02 16:20:49 -0500

    Update r_spec.rb

--- spec/classes/r_spec.rb
+++ spec/classes/r_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'r' do
   it do
-    should contain_package('R_2_15_3').with({
+    should contain_package('R_3_1_2').with({
       :provider => 'apple',
-      :source   => 'http://cran.rstudio.com/bin/macosx/R-2.15.3.pkg',
+      :source   => 'http://cran.rstudio.com/bin/macosx/R-3.1.2-mavericks.pkg',
     })
   end
 end
