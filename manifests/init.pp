9a160de8ac0d33e5d9b6c6a0b51dc1f7e1e701b8
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-11-02 16:17:51 -0500

    Update R source to the latest version
    Ideally we would install r via home-brew, but it seems to be impossible...

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@ class r {
   package { 'R_2_15_3':
     provider => 'apple',
     ensure => present,
-    source   => 'http://cran.rstudio.com/bin/macosx/R-2.15.3.pkg',
+    source   => 'http://cran.rstudio.com/bin/macosx/R-3.1.2-mavericks.pkg',
   }
 
 }
