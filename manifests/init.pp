4ce86528a2654e7bbf13cfdf29f07cec10a877e1
Kirill Sevastyanenko <kirill@urbancompass.com>
2014-11-03 15:43:22 -0500

    Fixed init. Now R installs properly into /usr/bin/R

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,13 @@
 # Uses http://cran.rstudio.com as source as this should redirect to a local mirror.
 class r {
 
-  package { 'R_3_1_2':
-    provider => 'compressed_pkg',
-    ensure => present,
-    source   => 'http://r.research.att.com/mavericks/R-3.1-branch/R-3.1-branch-mavericks-sa-x86_64.tar.gz',
+  exec { 'Download R':
+    command => $command,
+    before => Package['Install R']
   }
-
+  package { 'Install R':
+    provider => 'apple',
+    ensure   => present,
+    source   => '/opt/boxen/cache/R.pkg',
+  }
 }
