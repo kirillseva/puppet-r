91279588cf726c849a9ea1f8af53ee3ce1d9a2cd
Kirill Sevastyanenko <kirill@urbancompass.com>
2014-11-03 15:54:07 -0500

    forgot the most important line

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,7 @@
 #
 # Uses http://cran.rstudio.com as source as this should redirect to a local mirror.
 class r {
+  $command = "curl 'http://cran.rstudio.com/bin/macosx/R-3.1.2-mavericks.pkg' -L -q -o '/opt/boxen/cache/R.pkg'"
 
   exec { 'Download R':
     command => $command,
